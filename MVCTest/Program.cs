c49e274898626875d09732f47e5d709e9b841e35
Christine <zosachristinemarie@gmail.com>
2022-05-10 23:35:40 +0800

    Changed database exists checking

--- MVCTest/Program.cs
+++ MVCTest/Program.cs
@@ -4,28 +4,36 @@ using MVCTest.Data;
 using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectString = builder.Configuration.GetConnectionString("MVCTestContext");
+var connString = builder.Configuration.GetConnectionString("CheckExists");
+var dbConnString = builder.Configuration.GetConnectionString("MVCTestContext");
 var databaseName = "MVCTestContext-9682b5ae-3fa3-4b3c-a2f9-80cdcb5b6484";
 
 // Check if database exists in the computer. If it does not, create the database
-SqlConnection dbConnection = new SqlConnection(connectString);
-var command = new SqlCommand($"SELECT db_id('" + databaseName + "')", dbConnection);
+SqlConnection conn = new SqlConnection(connString);
+var command = new SqlCommand($"SELECT db_id('" + databaseName + "')", conn);
 
-dbConnection.Open();
+conn.Open();
 
 if (command.ExecuteScalar() == DBNull.Value)
 {
-    command = new SqlCommand($"CREATE DATABASE [" + databaseName + "] ON PRIMARY( NAME = " + databaseName + "_data, FILENAME = 'C:\\" + databaseName + "_data.mdf') LOG ON( NAME = " + databaseName + "_log, FILENAME = 'C:\\" + databaseName + "_log.ldf'", dbConnection);
+    SqlConnection dbConn = new SqlConnection(dbConnString);
+    dbConn.Open();
+    command = new SqlCommand($"CREATE DATABASE [" + databaseName + "] ON PRIMARY( NAME = " + databaseName + "_data, FILENAME = 'C:\\" + databaseName + "_data.mdf') LOG ON( NAME = " + databaseName + "_log, FILENAME = 'C:\\" + databaseName + "_log.ldf'", dbConn);
     command.ExecuteNonQuery();
+
+    if (dbConn.State == ConnectionState.Open)
+    {
+        dbConn.Close();
+    }
 }
 
-if (dbConnection.State == ConnectionState.Open)
+if (conn.State == ConnectionState.Open)
 {
-    dbConnection.Close();
+    conn.Close();
 }
 
 builder.Services.AddDbContext<MVCTestContext>(options =>
-    options.UseSqlServer(connectString ?? throw new InvalidOperationException("Connection string 'MVCTestContext' not found.")));
+    options.UseSqlServer(dbConnString ?? throw new InvalidOperationException("Connection string 'MVCTestContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
