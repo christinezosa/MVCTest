f4b1433b16a461d90c7805bf9be7e14ca637a675
Christine <zosachristinemarie@gmail.com>
2022-05-10 23:13:36 +0800

    Added code for checking the existence of the MVCTest database in localdb and creating it if it doesn't exist

--- MVCTest/Program.cs
+++ MVCTest/Program.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using MVCTest.Data;
-using MVCTest.Models;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectString = builder.Configuration.GetConnectionString("MVCTestContext");
+var databaseName = "MVCTestContext-9682b5ae-3fa3-4b3c-a2f9-80cdcb5b6484";
+
+// Check if database exists in the computer. If it does not, create the database
+SqlConnection dbConnection = new SqlConnection(connectString);
+var command = new SqlCommand($"SELECT db_id('" + databaseName + "')", dbConnection);
+
+dbConnection.Open();
+
+if (command.ExecuteScalar() == DBNull.Value)
+{
+    command = new SqlCommand($"CREATE DATABASE [" + databaseName + "] ON PRIMARY( NAME = " + databaseName + "_data, FILENAME = 'C:\\" + databaseName + "_data.mdf') LOG ON( NAME = " + databaseName + "_log, FILENAME = 'C:\\" + databaseName + "_log.ldf'", dbConnection);
+    command.ExecuteNonQuery();
+}
+
+if (dbConnection.State == ConnectionState.Open)
+{
+    dbConnection.Close();
+}
 
 builder.Services.AddDbContext<MVCTestContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCTestContext") ?? throw new InvalidOperationException("Connection string 'MVCTestContext' not found.")));
+    options.UseSqlServer(connectString ?? throw new InvalidOperationException("Connection string 'MVCTestContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
